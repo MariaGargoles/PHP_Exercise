cd6fd0a777386a22d9579a7f422d4a799d1c7919
MariaGargoles <mariagargoles@live.com>
2024-10-07 16:38:34 +0200

    ADD: add exercise 1 and 50% 2

---
+++ index1.php
@@ -0,0 +1,27 @@
+<?php
+$rooms = array(
+    array(
+        'ID' => 1,
+        'Name' => 'Single',
+        'Number' => 101,
+        'Price' => 90,
+        'Discount' => 10,
+    ),
+    array(
+        'ID' => 2,
+        'Name' => 'Double',
+        'Number' => 102,
+        'Price' => 160,
+        'Discount' => 20,
+    ),
+    array(
+        'ID' => 3,
+        'Name' => 'Suite',
+        'Number' => 103,
+        'Price' => 280,
+        'Discount' => 15,
+    ),
+);
+echo '<pre>';
+print_r($rooms);
+echo '</pre>';
---
+++ index2.php
@@ -0,0 +1,6 @@
+<?php
+
+$json_booking = 'Booking.json';
+$json_contact = 'ContactMessages.json';
+$json_rooms = 'Rooms.json';
+$json_users = 'Users.json';
