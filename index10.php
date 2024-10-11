e727438122d76748ba70f122fa9a57c9ee89aac1
MariaGargoles <mariagargoles@live.com>
2024-10-11 10:04:43 +0200

    fix

--- index10.php
+++ index10.php
@@ -1,36 +1,4 @@
 <?php
-
-require 'BladeOne.php';
-
-use eftec\bladeone\BladeOne;
-
-
-$views = __DIR__ . '/views';
-$cache = __DIR__ . '/cache';
-$blade = new BladeOne($views, $cache, BladeOne::MODE_AUTO);
-
-
-$mysqli = new mysqli("localhost", "root", "Segwanda12", "miranda");
-if ($mysqli->connect_errno) {
-    echo "Error al conectar con MySQL: " . $mysqli->connect_error;
-    exit();
-}
-
-
-$sql = "SELECT * FROM rooms";
-$rooms = $mysqli->query($sql);
-
-if (!$rooms) {
-    echo "Error en la consulta: " . $mysqli->error;
-    exit();
-}
-
-
-$roomsArray = [];
-while ($row = $rooms->fetch_assoc()) {
-    $roomsArray[] = $row;
-}
-$mysqli->close();
-
-
-echo $blade->run("rooms", ["rooms" => $roomsArray]);
+    require_once('./setup.php');
+    echo $blade->run('rooms',array('rooms'=>$rooms));
+    ?>
